ea250babdc4c98cd43fb546f395525cdf48bfa57
hideposu <165666671+hideposu@users.noreply.github.com>
2024-06-06 20:32:59 +0900

    Add files via upload

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,19 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+	       System.out.println("Aさんの点は" + 10 + "点");
+	       System.out.println("Bさんの点は" + 20 + "点");
+	       System.out.println("Cさんの点は" + 30 + "点");
+	       System.out.println("Dさんの点は" + 40 + "点");
+	       System.out.println("Eさんの点は" + 50 + "点");
+	       System.out.println("Fさんの点は" + 60 + "点");
+	       System.out.println("Gさんの点は" + 70 + "点");
+	       System.out.println("Hさんの点は" + 80 + "点");
+	       System.out.println("Iさんの点は" + 90 + "点");
+	       System.out.println( "Jさんの点は" + 100 + "点");
+	       System.out.println( "テストの平均点は"+((10 + 20 +30 +40 +50 +60 +70 +80 +90 + 100)/10)+"点");
+	}
+
+}
